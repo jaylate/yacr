f3c814e9c367db9be19c2087724db8f18d1fc832
jaylate <178095084+jaylate@users.noreply.github.com>
2026-01-22 23:56:29 +0100

    Add bare-bones execution in isolated user and UTS namespaces

---
+++ namespace.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+    "log"
+    "os"
+    "os/exec"
+    "syscall"
+)
+
+func executeInNamespace(name string, arg ...string) {
+    cmd := exec.Command(name)
+    cmd.SysProcAttr = &syscall.SysProcAttr {
+	Cloneflags: syscall.CLONE_NEWUSER | syscall.CLONE_NEWUTS,
+	UidMappings: []syscall.SysProcIDMap {
+	    {
+                ContainerID: 0,
+	        HostID: os.Getuid(),
+		Size: 1,
+	    },
+	},
+	GidMappings: []syscall.SysProcIDMap {
+	    {
+                ContainerID: 0,
+	        HostID: os.Getgid(),
+		Size: 1,
+	    },
+	},
+    }
+
+    cmd.Stdout = os.Stdout
+    cmd.Stdin = os.Stdin
+    cmd.Stderr = os.Stderr
+
+    err := cmd.Start()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    err = cmd.Wait()
+    if err != nil {
+        log.Fatal(err)
+    }
+}
