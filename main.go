ca8ef68e4579888a39b469d0a5c1fbd1de366c7d
poyo86 <232233992+poyo86@users.noreply.github.com>
2026-01-23 16:35:25 +0100

    Parse arguments and execute them in namespace

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-    executeInNamespace("bash", "")
+	usageMsg := "Usage: " + os.Args[0] + " run <command> <arguments>"
+	if len(os.Args) > 1 {
+		if os.Args[1] == "run" && len(os.Args) > 2 {
+			command := os.Args[2]
+			args := os.Args[3:]
+			executeInNamespace(command, args...)
+		} else {
+			fmt.Println("Incorrect usage")
+			fmt.Println(usageMsg)
+		}
+	} else {
+		fmt.Println(usageMsg)
+	}
 }
