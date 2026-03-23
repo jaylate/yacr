372351f24e8c713f969aa0ae651f804f61d45810
jaylate <178095084+jaylate@users.noreply.github.com>
2026-03-24 00:52:49 +0100

    test(cgroups): add coverage without implementation (CI workflow should fail)

---
+++ runtime/resources/cgroups.go
@@ -0,0 +1,33 @@
+package resources
+
+type ResourceLimits struct {
+	MemoryBytes int64   // 0 = unlimited
+	CPUCores    float64 // 0 = unlimited
+	PIDsMax     int     // 0 = unlimited
+}
+
+type CgroupManager interface {
+	Create(id string, limits ResourceLimits) error
+	AddProcess(pid int) error
+	Destroy(id string) error
+}
+
+type CgroupsManager struct {
+	basePath string
+	limits   ResourceLimits
+}
+
+func NewCgroupsManager(basePath string, limits ResourceLimits) *CgroupsManager {
+	return &CgroupsManager{basePath, limits}
+}
+
+func (m *CgroupsManager) Create(id string, limits ResourceLimits) error { return nil }
+func (m *CgroupsManager) AddProcess(pid int) error                      { return nil }
+func (m *CgroupsManager) Destroy(id string) error                       { return nil }
+
+func ParseMemoryString(s string) (int64, bool) { return -1, false }
+func ParseCPUString(s string) (string, bool)   { return "", false }
+func ParsePIDsString(s string) (int, bool)     { return 0, false }
+func DetectCgroupVersion() string              { return "" }
+func DetectUserCgroupPath() string             { return "" }
+func IsCgroupWritable(path string) bool        { return false }
